ac4c415a3849da1236c66aacf1603e03fed9a8dd
fuuka223 <putarous@gmail.com>
2026-04-01 08:58:03 +0900

    deviseコントローラーの設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,24 @@
 class ApplicationController < ActionController::Base
+  # Basic認証
   before_action :basic_auth
+
+  # devise
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
 
+  # Basic認証
   def basic_auth
     return unless Rails.env.production?
 
     authenticate_or_request_with_http_basic do |username, password|
       username == ENV['BASIC_AUTH_USER'] && password == ENV['BASIC_AUTH_PASSWORD']
     end
+  end
+
+  # devise
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up,
+                                      key: [:nickname, :last_name, :first_name, :last_name_kana, :first_name_kana, :birth_date])
   end
 end
