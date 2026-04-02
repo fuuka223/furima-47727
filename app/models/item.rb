37da92349660cd95abc599c4156a5189f6b83d56
fuuka223 <putarous@gmail.com>
2026-04-02 16:24:21 +0900

    アソシエーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,10 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtentions
+  belongs_to :category
+  belongs_to :sales_status
+  belongs_to :shipping_fee_status
+  belongs_to :prefecture
+  belongs_to :scheduled_delivery
+
   belongs_to :user
 end
