bf06d787dc46ccb0d879a0e6c0846654012751cc
fuuka223 <putarous@gmail.com>
2026-04-02 17:14:44 +0900

    コントローラーの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,19 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.save(item_params)
+  end
+
+  private
+
+  def item_params
+    params.require(:Item).peremit(:image, :name, :info, :category_id, :sales_status, :shipping_fee_status, :prefecture,
+                                  :scheduled_delivery, :price).merge(user_id: current_user.id)
+  end
 end
