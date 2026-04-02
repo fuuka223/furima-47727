a31e9b9e6e2a5b4783a6bf02b80b08b1390d2291
fuuka223 <putarous@gmail.com>
2026-04-02 15:07:04 +0900

    activ hashのモデル作成

---
+++ app/models/prefecure.rb
@@ -0,0 +1,11 @@
+class Prefecture < ActiveHash::Base
+  self.data = [
+    { id: 1, name: '---' },
+    { id: 2, name: '' },
+    { id: 3, name: '' },
+    { id: 4, name: '' }
+  ]
+
+  include ActiveHash: :Associations
+  has_many :Items
+end
