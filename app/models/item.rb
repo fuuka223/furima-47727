5c192c6a244692236e1224474581cdf17c81a073
fuuka223 <putarous@gmail.com>
2026-04-02 16:50:38 +0900

    画像のアソシエーションとバリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,8 +6,9 @@ class Item < ApplicationRecord
   belongs_to :prefecture
   belongs_to :scheduled_delivery
   belongs_to :user
+  has_one_attached :image
 
-  validates :name, :info, :price, presence: true
+  validates :image, :name, :info, :price, presence: true
   validates :category_id, :sales_status, :shipping_fee_status, :prefecture, :scheduled_delivery,
             numericality: { other_than: 1, message: "can't be blank" }
   validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
